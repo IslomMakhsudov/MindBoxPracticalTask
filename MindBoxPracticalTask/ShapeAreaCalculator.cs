fbee89c6ebb3008ab84661ddf4da1d8aa24a5c60
Islom Makhsudov <81351589+IslomMakhsudov@users.noreply.github.com>
2024-08-28 20:44:58 +0500

    Create ShapeAreaCalculator.cs

---
+++ MindBoxPracticalTask/ShapeAreaCalculator.cs
@@ -0,0 +1,10 @@
+﻿using GeometryCalculatorLib.Interfaces;
+
+namespace GeometryCalculatorLib;
+public class ShapeAreaCalculator
+{
+    public static double CalculateArea(IShape shape)
+    {
+        return shape.GetArea();
+    }
+}
