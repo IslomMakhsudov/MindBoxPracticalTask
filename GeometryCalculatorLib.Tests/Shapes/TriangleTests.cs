4060cefd48e670e5fafffc32fac312bb0da0a1f9
Islom Makhsudov <81351589+IslomMakhsudov@users.noreply.github.com>
2024-08-28 22:00:16 +0500

    Create TriangleTests.cs

---
+++ GeometryCalculatorLib.Tests/Shapes/TriangleTests.cs
@@ -0,0 +1,45 @@
+﻿using GeometryCalculatorLib.Shapes;
+
+namespace GeometryCalculatorLib.Tests.Shapes;
+
+[TestFixture]
+public class TriangleTests
+{
+    [Test]
+    public void GetArea_ValidSides_ReturnsCorrectTriangleArea()
+    {
+        var triangle = new Triangle(3, 4, 5);
+
+        var area = triangle.GetArea();
+
+        Assert.That(area, Is.EqualTo(6));
+    }
+
+    [Test]
+    public void IsRightTriangle_ValidRightTriangle_ReturnsTrue()
+    {
+        var triangle = new Triangle(3, 4, 5);
+
+        var isRightTriangle = triangle.IsRightTriangle();
+
+        Assert.That(isRightTriangle, Is.True);
+    }
+
+    [Test]
+    public void TriangleConstructor_InvalidSides_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+        {
+            var triangle = new Triangle(1, 2, 3);
+        });
+    }
+
+    [Test]
+    public void TriangleConstructor_NegativeSide_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+        {
+            var triangle = new Triangle(3, 4, -5);
+        });
+    }
+}
