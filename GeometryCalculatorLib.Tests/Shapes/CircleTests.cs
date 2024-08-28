48867d10a84ab0053365994678ad3a27d3217336
Islom Makhsudov <81351589+IslomMakhsudov@users.noreply.github.com>
2024-08-28 22:00:14 +0500

    Create CircleTests.cs

---
+++ GeometryCalculatorLib.Tests/Shapes/CircleTests.cs
@@ -0,0 +1,26 @@
+﻿using GeometryCalculatorLib.Shapes;
+
+namespace GeometryCalculatorLib.Tests.Shapes;
+
+[TestFixture]
+public class CircleTests
+{
+    [Test]
+    public void GetArea_RadiusIsPositive_ReturnsCorrectCircleArea()
+    {
+        var circle = new Circle(5);
+
+        var area = circle.GetArea();
+
+        Assert.That(area, Is.EqualTo(Math.PI * 25));
+    }
+
+    [Test]
+    public void CircleConstructor_RadiusIsNegative_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+        {
+            var circle = new Circle(-1);
+        });
+    }
+}
