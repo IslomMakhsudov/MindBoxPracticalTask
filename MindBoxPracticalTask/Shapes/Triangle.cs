aba124adb6f3afc3a06c1de668796578e734aeea
Islom Makhsudov <81351589+IslomMakhsudov@users.noreply.github.com>
2024-08-28 20:44:56 +0500

    Create Triangle.cs

---
+++ MindBoxPracticalTask/Shapes/Triangle.cs
@@ -0,0 +1,39 @@
+﻿using GeometryCalculatorLib.Interfaces;
+
+namespace GeometryCalculatorLib.Shapes;
+public class Triangle : IShape
+{
+    public double SideA { get; }
+    public double SideB { get; }
+    public double SideC { get; }
+
+    public Triangle(double sideA, double sideB, double sideC)
+    {
+        if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+            throw new ArgumentException("All sides should be positive numbers.");
+        if (!IsValidTriangle(sideA, sideB, sideC))
+            throw new ArgumentException("The sides cannot form a triangle.");
+
+        SideA = sideA;
+        SideB = sideB;
+        SideC = sideC;
+    }
+
+    public double GetArea()
+    {
+        var s = (SideA + SideB + SideC) / 2;
+        return Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+    }
+
+    public bool IsRightTriangle()
+    {
+        var sides = new[] { SideA, SideB, SideC };
+        Array.Sort(sides);
+        return Math.Abs(Math.Pow(sides[2], 2) - (Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2))) < 1e-10;
+    }
+
+    private static bool IsValidTriangle(double a, double b, double c)
+    {
+        return a + b > c && a + c > b && b + c > a;
+    }
+}
