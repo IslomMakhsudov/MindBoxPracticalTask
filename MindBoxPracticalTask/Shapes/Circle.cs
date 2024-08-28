4b72c6b25ec30b7f95842ee6198b532bfef2c336
Islom Makhsudov <81351589+IslomMakhsudov@users.noreply.github.com>
2024-08-28 20:33:33 +0500

    Create Circle.cs

---
+++ MindBoxPracticalTask/Shapes/Circle.cs
@@ -0,0 +1,19 @@
+﻿using GeometryCalculatorLib.Interfaces;
+
+namespace GeometryCalculatorLib.Shapes;
+public class Circle : IShape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        if (radius <= 0)
+            throw new ArgumentException("Radius should be a positive number.");
+        Radius = radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * Math.Pow(Radius, 2);
+    }
+}
