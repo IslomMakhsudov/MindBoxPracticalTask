71982359979636f1e7cec8e5894931a9c854db13
Islom Makhsudov <islommahsudov20@gmail.com>
2024-08-30 16:52:33 +0500

    Create ShapeAreaCalculatorTests.cs

---
+++ GeometryCalculatorLib.Tests/ShapeAreaCalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using GeometryCalculatorLib.Interfaces;
+using GeometryCalculatorLib.Shapes;
+
+namespace GeometryCalculatorLib.Tests;
+[TestFixture]
+public class ShapeAreaCalculatorTests
+{
+    [Test]
+    public void CalculateArea_ShapeIsCircle_ReturnsCorrectArea()
+    {
+        IShape circle = new Circle(5);
+
+        var area = ShapeAreaCalculator.CalculateArea(circle);
+
+        Assert.That(area, Is.EqualTo(Math.PI * 25));
+    }
+
+    [Test]
+    public void CalculateArea_ShapeIsTriangle_ReturnsCorrectArea()
+    {
+        IShape triangle = new Triangle(3, 4, 5);
+
+        var area = ShapeAreaCalculator.CalculateArea(triangle);
+
+        Assert.That(area, Is.EqualTo(6));
+    }
+}
